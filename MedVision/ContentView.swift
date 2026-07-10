99d970a7736c12fd30a5616cafa2c3b2d2bc0958
Nanond Nimitkul <nanond.nimitkul@gmail.com>
2026-07-10 08:41:43 +0700

    Initial Commit

---
+++ MedVision/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MedVision
+//
+//  Created by Nanond Nimitkul on 10/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MedVision/MedVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedVisionApp.swift
+//  MedVision
+//
+//  Created by Nanond Nimitkul on 10/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MedVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
