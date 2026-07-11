3d84aff084904385655cbee2418c96f86be8b5dd
Nanond Nimitkul <nanond.nimitkul@gmail.com>
2026-07-11 22:28:13 +0700

    Changes made: 1. Added splashscreen with placeholder text 2. Added onboarding screen with placeholder text 3. Added proper logic to onboarding screen, where it will only show up once every time the app is rebuilt

--- MedVision/App/ContentView.swift
+++ MedVision/App/ContentView.swift
@@ -1,28 +1,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showSplash = true
+    @AppStorage("shouldShowOnboarding") private var shouldShowOnboarding = true
+    
     var body: some View {
-        TabView {
-            TodayView()
-                .tabItem {
-                    Label("Today", systemImage: "bell.fill")
+        VStack {
+            if showSplash {
+                SplashScreenView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline:.now() + 2) {
+                            withAnimation {
+                                    showSplash = false
+                            }
+                        }
+                    }
+            }
+            else if shouldShowOnboarding {
+                OnboardingView()
+            }
+            else {
+                TabView {
+                    TodayView()
+                        .tabItem {
+                            Label("Today", systemImage: "bell.fill")
+                        }
+                    MedicinesView()
+                        .tabItem {
+                            Label("Medicines", systemImage: "pills.fill")
+                        }
+                    ScanView()
+                        .tabItem {
+                            Label("Scan", systemImage: "camera.fill")
+                        }
+                    HistoryView()
+                        .tabItem {
+                            Label("History", systemImage: "calendar")
+                        }
+                    ProfileView()
+                        .tabItem {
+                            Label("Profile", systemImage: "gear")
+                        }
                 }
-            MedicinesView()
-                .tabItem {
-                    Label("Medicines", systemImage: "pills.fill")
-                }
-            ScanView()
-                .tabItem {
-                    Label("Scan", systemImage: "camera.fill")
-                }
-            HistoryView()
-                .tabItem {
-                    Label("History", systemImage: "calendar")
-                }
-            ProfileView()
-                .tabItem {
-                    Label("Profile", systemImage: "gear")
-                }
+            }
         }
     }
 }
